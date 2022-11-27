d3cea8d3d4be924f499e10d2bc7275fcaf07d787
Tianshuo Cong <90805377+tianshuocong@users.noreply.github.com>
2022-11-27 22:38:35 +0100

    Update model_para.py

--- model_para.py
+++ model_para.py
@@ -20,3 +20,12 @@ for key, value in cp_base.items():
 
 base_model.load_state_dict(new_state_dict)
 base_model = base_model.to(device)
+
+
+## replace BN to GN
+
+for name, module in net.named_modules():
+    if isinstance(module, nn.BatchNorm2d):
+        bn = get_layer(net, name)
+        gn = nn.GroupNorm(8, bn.num_features)
+        set_layer(net, name, gn)
