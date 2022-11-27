6159d26ee8c8848a58855ec1b1d72a7cf1eab41a
Tianshuo Cong <90805377+tianshuocong@users.noreply.github.com>
2022-11-27 02:19:02 +0100

    Update model_para.py

--- model_para.py
+++ model_para.py
@@ -1,6 +1,22 @@
-
+## para name
 for paras in model.state_dict():
   print(paras)
   
-  
 print(model.state_dict()['bn1.running_mean'])
+
+
+## change para name
+base_model = resnet18(pretrained=False).to(device)
+
+cp_base = torch.load("./ckpt-target/resnet18-cinic.ckpt", map_location=device)['state_dict']
+## ['state_dict'] (hyperparameters)
+
+from collections import OrderedDict
+new_state_dict = OrderedDict()
+
+for key, value in cp_base.items():
+    key = key[6:] # remove `model.`
+    new_state_dict[key] = value
+
+base_model.load_state_dict(new_state_dict)
+base_model = base_model.to(device)
